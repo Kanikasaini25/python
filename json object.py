e08e1f0bce2c0c75a8d5fbc0d36a8803b013be9e
kanika saini <89476583+Kanikasaini25@users.noreply.github.com>
2022-01-23 15:58:16 +0530

    Update json object.py

--- json object.py
+++ json object.py
@@ -1,2 +1,28 @@
-def m(n):
-    print(n*n)
+book={}
+book['a']=
+{
+    "name":"k",
+    "phn":12345678',
+    "add":"dfgh"
+}
+book['b']=
+{
+    "name":"h",
+    "phn":1234567878',
+    "add":"dfg4567"
+}
+import json
+a=json.dumps(book)
+with open("c/user/kanika saini/json.txt","w") as f: #string format
+    f.write(a)
+    
+...........read the file
+with open("c/user/kanika saini/json.txt","r") as f:
+    f.read()  # string format
+    
+    
+import json
+k=json.load(book)
+print(k) # dict format
+    
+    
